06a94555a266122435dd8e0d89ad524f9d4f1f60
Fernando de Barros <108085545+fernandobarrosd@users.noreply.github.com>
2024-02-08 13:22:21 -0300

    Atualizar o NoteCard.tsx

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -49,16 +49,31 @@ export function NoteCard({ note: { id, createdDate, content }  } : NoteProps) {
             text-left outline-none
             hover:ring-2 hover:ring-slate-600
             focus-visible:ring-2 focus-visible:ring-lime-400">
-              <NoteCardContent note={{ createdDate, content }}>
-                <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t
-                from-black/60 to-black/0 pointer-events-none"/>
-              </NoteCardContent>
+              <div className="flex flex-col gap-4 overflow-auto pb-4">
+       <span className="font-medium text-sm text-slate-300">
+        { formatDistanceToNow(createdDate, { locale: ptBR, addSuffix: true })
+        .replace(/^[a-z]/, e => e.toUpperCase())}
+      </span>
+      <p className="font-medium text-xs text-slate-400 leading-6">
+        { content }
+      </p>
+      <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t
+    from-black/60 to-black/0 pointer-events-none"/>
+    </div>
+              
               
             </ReactDialog.Trigger>
             <ReactDialog.Portal>
               <Dialog.Overlay/>
               <Dialog.Content>
-                <NoteCardContent note={{ createdDate, content }}>
+                <div className="flex flex-col gap-4 overflow-auto pb-4">
+       <span className="font-medium text-sm text-slate-300">
+        { formatDistanceToNow(createdDate, { locale: ptBR, addSuffix: true })
+        .replace(/^[a-z]/, e => e.toUpperCase())}
+      </span>
+      <p className="font-medium text-xs text-slate-400 leading-6">
+        { content }
+      </p>
                   <Dialog.CloseButton />
                   <ReactDialog.Close className="bg-slate-800 text-slate-300 absolute w-full 
                   left-[0] right-[0] bottom-0 px-16 py-4 text-xs group"
